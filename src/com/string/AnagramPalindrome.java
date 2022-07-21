5a0af7a3fb412383b5c8a0a281642f46f7be4a18
Naman Ajmera <cool.naman.ajmera@gmail.com>
2022-07-21 08:11:18 +0530

    Added: Starting of Anagram Palindrome

---
+++ src/com/string/AnagramPalindrome.java
@@ -0,0 +1,13 @@
+package com.string;
+
+import java.util.Scanner;
+
+public class AnagramPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        System.out.println("str1 ==> " + str1);
+
+
+    }
+}
